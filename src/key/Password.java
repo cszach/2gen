9e242b5ef169f119876100c575f04fa6391ff89a
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-08-08 23:30:16 +0700

    Add classes for keys

---
+++ src/key/Password.java
@@ -0,0 +1,27 @@
+package key;
+
+import supply.CharacterLibrary;
+import java.util.concurrent.ThreadLocalRandom;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class Password {
+	private String value; // The password itself
+	
+	// Attributes of a password
+	// Used for displaying password's details
+	private int length;
+	private boolean upperCase;
+	private boolean lowerCase;
+	private boolean number;
+	private boolean symbol;
+	
+	Password(int length, boolean upperCase, boolean lowerCase, boolean number, boolean symbol, String[] excludeKeyFileName) {
+		this.length = length;
+		this.upperCase = upperCase;
+		this.lowerCase = lowerCase;
+		this.number = number;
+		this.symbol = symbol;
+	}
+}
---
+++ src/key/Pin.java
@@ -0,0 +1,60 @@
+package key;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class Pin {
+	private String value;  // The pin itself
+	private int length;  // Number of digits
+
+	Pin(int length, String[] excludeKeyFileName) {
+		this.length = length;
+		String value = "";
+
+		// TODO: Generate random pin and check if it matches any exception
+		mainLoop:
+			while (true) {
+
+				// TODO: Generate random pin
+				for (int i = 0; i < length; i++) {
+					int flag = ThreadLocalRandom.current().nextInt(1, 10);
+					value += Integer.toString(flag);
+				}
+
+				this.value = value;  // Assign the generated pin to the pin's value
+
+				if (excludeKeyFileName == null) {  // No exception is set
+					break mainLoop;
+				}
+
+				// TODO: Scan files to check matches
+				// There's custom file(s) for scanning -> scan to see if there's a match
+				for (String fileName : excludeKeyFileName) {
+					try {
+						File keyFile = new File(fileName);
+						Scanner reader = new Scanner(keyFile);
+						while (reader.hasNextLine()) {
+							String flagKey = reader.nextLine();
+							if (flagKey == this.value) {  // Generated pin matches with an exception
+								reader.close();
+								continue mainLoop;  // Continue the while loop
+							}
+							reader.close();
+						}
+					}
+					catch (FileNotFoundException e) {
+						System.err.println("File " + fileName + " not found.");
+						// Continue with the next files
+					}
+				}
+
+				break mainLoop;  // Only executed if all files are checked and no exceptions match
+			}
+
+
+
+		value = null;
+	}
+}
