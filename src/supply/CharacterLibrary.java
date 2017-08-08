3a3dfb6f03220a50dd88f83d19f13167ab6b4ff1
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-08-08 23:31:23 +0700

    Add supplier for class Password

---
+++ src/supply/CharacterLibrary.java
@@ -0,0 +1,8 @@
+package supply;
+
+public class CharacterLibrary {
+	final static String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	final static String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+	final static String NUMBER = "0123456789";
+	final static String SYMBOL = "~`!@#$%^&*()-_+={}[]|\\;:\"<>,./?";
+}
