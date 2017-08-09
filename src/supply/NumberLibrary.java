206901d3feee2af7f377a49b81e9bf5edc390ecc
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-08-09 23:17:09 +0700

    Add method for getting number of lines in a file

--- src/supply/NumberLibrary.java
+++ src/supply/NumberLibrary.java
@@ -1,9 +1,32 @@
 package supply;
 
 import java.util.concurrent.ThreadLocalRandom;  // For Java 1.7+
+import java.io.File;
+import java.io.FileReader;
+import java.io.LineNumberReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class NumberLibrary {
 	public static int randomNumber(int min, int max) {
 		return ThreadLocalRandom.current().nextInt(min, max + 1);
+	}
+	
+	public static int numberOfLines(String fileName) throws IOException {
+		int flag = 0;
+		LineNumberReader result = null;
+		try {
+			result = new LineNumberReader(new FileReader(new File(fileName)));
+			result.skip(Long.MAX_VALUE);
+			flag = result.getLineNumber() + 1;
+		} 
+		catch (FileNotFoundException e) {
+		}
+		finally {
+			if (result != null) {
+				result.close();
+			}
+		}
+		return flag;
 	}
 }
