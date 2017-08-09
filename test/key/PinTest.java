cf204d1cb190188a16c1d40b83d752bd87010a23
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-08-09 23:18:22 +0700

    Update test for pin generation

--- test/key/PinTest.java
+++ test/key/PinTest.java
@@ -1,21 +1,34 @@
 package key;
 
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import supply.NumberLibrary;
+
 
 public class PinTest {
 	Pin myPin;
 
 	@Before
 	public void setUp() {
-		String[] fileList1 = {"samples/pinlist1.txt"};
+		String[] fileList1 = {"samples/pinlist1.txt", "samples/pinlist2.txt"};
 		myPin = new Pin(4, fileList1);
 	}
 
 	@Test
 	public void test() {
+		try {
+			for (int i = 0; i < 1000; i++) {
+				assertTrue(myPin.value != 
+						Files.readAllLines(Paths.get("/samples/pinlist1.txt"))
+						.get(NumberLibrary.randomNumber(1, NumberLibrary.numberOfLines("/sample/pinlist1.txt"))));				
+			}
+		} catch (IOException e) {
+			// Do nothing
+		}
 	}
 
 }
