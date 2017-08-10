3a0ba6120504950c7b49ddad537972c31b4b3a64
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-08-10 18:56:57 +0700

    Finish constructor for Password

--- src/key/Password.java
+++ src/key/Password.java
@@ -1,11 +1,12 @@
 package key;
 
 import supply.CharacterLibrary;
-import supply.NumberLibrary;
+import org.apache.commons.lang3.RandomStringUtils;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+@SuppressWarnings("deprecation")
 public class Password {
 	private String value; // The password itself
 
@@ -19,10 +20,54 @@ public class Password {
 
 	Password(int length, boolean upperCase, boolean lowerCase, boolean number, boolean symbol,
 			String[] excludeKeyFileName) {
+		// TODO: Assign attributes
 		this.length = length;
 		this.upperCase = upperCase;
 		this.lowerCase = lowerCase;
 		this.number = number;
 		this.symbol = symbol;
+		
+		// TODO: Generate password and check exceptions
+		mainLoop: while (true) {
+			// TODO: Generate password
+			String charInPassword = "";
+			if (upperCase) { charInPassword += CharacterLibrary.UPPERCASE; }
+			if (lowerCase) { charInPassword += CharacterLibrary.LOWERCASE; }
+			if (number) { charInPassword += CharacterLibrary.NUMBER; }
+			if (symbol) { charInPassword += CharacterLibrary.SYMBOL; }
+			this.value = RandomStringUtils.random(length, charInPassword);
+			
+			if (excludeKeyFileName == null) {  // No exception is set
+				break mainLoop;
+			}
+			
+			// TODO: Scan file(s) to check for exceptions
+			for (String fileName : excludeKeyFileName) {
+				try {
+					File keyFile = new File(fileName);
+					Scanner reader = new Scanner(keyFile);
+					while (reader.hasNextLine()) {
+						String flagKey = reader.nextLine();
+						if (flagKey == this.value) { // Generated password matches with an exception
+							reader.close();
+							continue mainLoop; // Continue the while loop
+						}
+					}
+					reader.close();
+				} 
+				catch (FileNotFoundException e) {
+					System.err.println("File " + fileName + " not found.");
+					// Continue with the next files
+				}
+			}
+			charInPassword = null;
+			break mainLoop;  // Executed only when all files are checked and no exceptions is hit
+		}
+	}
+
+	public static void main (String[] args) {
+		Password myPassword = new Password(12, true, true, true, true, null);
+		System.out.println(myPassword.value);
+		System.out.println(myPassword.length);
 	}
 }
