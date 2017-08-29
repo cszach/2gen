fc007946a4e5d8df2f2a2ed2b182e42e7191592a
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-08-29 21:20:36 +0700

    Initiate code for 2Gen GUI

---
+++ src/main/TwoGenG.java
@@ -0,0 +1,27 @@
+package main;
+
+import javax.swing.*;
+import dialog.UnsupportedOSWarn;
+
+/**
+ * This is a 2Gen distribution: 2Gen GUI
+ *
+ * @author Nguyen Hoang Duong (NOVAglow)
+ * @date 2017-29-08
+ */
+public class TwoGenG {
+    JFrame frame;
+    TwoGenG() {
+        frame = new JFrame();
+    }
+
+    public static void main (String[] args) {
+        // Check if OS is supported
+        // If not, shows warning
+        if (System.getProperty("os.name").equals("Windows") || System.getProperty("os.name").equals("Linux")) {
+            new UnsupportedOSWarn();
+        }
+
+        new TwoGenG();
+    }
+}
