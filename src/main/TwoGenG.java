10cb323382b132c64799fd63f10d1366b6294575
Nguyễn Hoàng Dương <you_create@protonmail.com>
2017-08-31 20:38:00 +0700

    Add outlines

--- src/main/TwoGenG.java
+++ src/main/TwoGenG.java
@@ -1,7 +1,7 @@
 package main;
 
 import javax.swing.*;
-// import dialog.UnsupportedOSWarn;
+import java.awt.Color;
 
 /**
  * This is a 2Gen distribution: 2Gen GUI
@@ -12,18 +12,25 @@ import javax.swing.*;
 public class TwoGenG {
     JFrame frame;
     TwoGenG() {
+        // Setup for frame
         frame = new JFrame("2Gen");
+        frame.setSize(800, 400);
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // TODO: Set frame's background color
+        Color backgroundColor = Color.decode("#C1DFD4");
+        frame.getContentPane().setBackground(backgroundColor);
+
+        // Output field for generated passwords / PINs
+        JTextArea keyOutput = new JTextArea();
+        frame.add(keyOutput);
+        keyOutput.setBounds(450, 10, 335, 380);  // Set position: on the left
+        keyOutput.setEditable(false);  // Make output field uneditable
     }
 
     public static void main (String[] args) {
-        // Check if OS is supported
-        // If not, shows warning
-        /*
-        if (System.getProperty("os.name").equals("Windows") || System.getProperty("os.name").equals("Linux")) {
-            new UnsupportedOSWarn();
-        }
-        */
-
         new TwoGenG();
     }
 }
